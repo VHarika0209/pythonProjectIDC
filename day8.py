26f9554e820af8aaeb6248b6169615353d6dc146
Vharika0209 <vikkurthiharika@gmail.com>
2025-06-05 03:02:42 +0530

    commiting day 8 file

---
+++ day8.py
@@ -0,0 +1,22 @@
+class Car:
+    def __init__(self, brand, model, year, color):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.color = color
+
+    def display_info(self):
+        print(f"Car Details:")
+        print(f"Brand : {self.brand}")
+        print(f"Model : {self.model}")
+        print(f"Year  : {self.year}")
+        print(f"Color : {self.color}")
+
+# Creating objects
+car1 = Car("Toyota", "Camry", 2020, "Blue")
+car2 = Car("Tesla", "Model 3", 2023, "White")
+
+# Displaying info
+car1.display_info()
+print()
+car2.display_info()
